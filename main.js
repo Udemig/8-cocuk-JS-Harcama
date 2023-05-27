bcde2231a6a9a88e7f1e2aa5f50103ab8fb795eb
furkanevin <furkanevin0@gmail.com>
2023-05-27 18:17:53 +0300

    ekleme ve silme

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
 const buton = document.getElementById('buton');
 const fiyatInput = document.getElementById('fiyat-inp');
 const harcamaInput = document.getElementById('harcama-inp');
+const liste = document.getElementById('harcamalar');
 
 buton.addEventListener('click', addExpense);
 
+// listedeki tıklanma olyını izler
+liste.addEventListener('click', handleDelete);
+
 function addExpense() {
-  alert(fiyatInput.value);
+  // liste elemanı oluşturma
+  const harcamaDiv = document.createElement('div');
+
+  // oluşan dive class verme
+  harcamaDiv.classList.add('harcama');
+
+  // divin içerğini değiştirme
+  harcamaDiv.innerHTML = `
+          <h1>${harcamaInput.value}</h1>
+          <h3>${fiyatInput.value} TL</h3>
+          <button id="sil-btn">Sil</button>
+  `;
+
+  // divin son halini listeye ekleme
+  liste.appendChild(harcamaDiv);
+}
+
+// backtick
+
+function handleDelete(ev) {
+  // tıklanan elemanı tespit etme
+  var element = ev.target;
+
+  // eğerki tıklanan elemanın id'si sil-btn ise
+  // o elemanın bir üst elemanını kaldır
+  if (element.id === 'sil-btn') {
+    element.parentElement.remove();
+  }
 }
